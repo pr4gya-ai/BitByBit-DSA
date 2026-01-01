f830adf5cb746116edd860eb3b903c30b46e47fe
Pragya Singh <pr4gy4.singh@gmail.com>
2026-01-01 09:10:26 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (89.12%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] plusOne(int[] digits) {
         int n = digits.length;
-        for(int i =  n-1;i>=0;i--) {
+        for(int i = n-1;i>=0;i--) {
             if (digits[i] + 1 != 10) {
                 digits[i]++;
                 return digits;

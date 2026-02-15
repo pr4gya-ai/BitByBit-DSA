fc9e85f165b69e0b48ded335217d32efbde82bc1
Pragya Singh <pr4gy4.singh@gmail.com>
2026-02-15 21:50:30 +0530

    Time: 1 ms (96.73%) | Memory: 42.7 MB (61.25%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int mySqrt(int x) {
+        int left = 0, right = x;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if ((long) mid * mid <= x) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return right;  
+    }
+}

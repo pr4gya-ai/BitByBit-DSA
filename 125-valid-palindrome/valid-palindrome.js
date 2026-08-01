38db1c0b14a4f15559325f6c4220eb73c512197a
Pragya Singh <214129397+pr4gya-ai@users.noreply.github.com>
2026-08-01 10:49:43 +0530

    Time: 5 ms (63.98%) | Memory: 58.4 MB (45.01%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -4,15 +4,9 @@
  */
 var isPalindrome = function(s) {
 
-    s = s.toLowerCase();
+    s = s.toLowerCase().replace(/[^a-z0-9]/g, "");
 
-    s = s.replace(/[^a-z0-9]/g, "");
+    let reverse = s.split("").reverse().join("");
 
-    let reversed = "";
-
-    for (let i = s.length - 1; i >= 0; i--) {
-        reversed += s[i];
-    }
-    
-    return reversed === s;
+    return s === reverse;
 };

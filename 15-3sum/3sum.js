8a0a0f82ee2817ca17ef1c984affec2b6f31c29f
PRAGYA SINGH <214129397+pr4gya-ai@users.noreply.github.com>
2026-08-11 23:12:21 +0530

    Time: 38 ms (45.03%) | Memory: 71 MB (93.69%) - LeetSync

---
+++ 15-3sum/3sum.js
@@ -0,0 +1,45 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+    let res = [];
+
+    nums.sort((a, b) => a - b);
+
+    for (let i = 0; i < nums.length - 2; i++) {
+        if (i > 0 && nums[i] === nums[i - 1]) {
+            continue;
+        }
+
+        let j = i + 1;
+        let k = nums.length - 1;
+
+        while (j < k) {
+
+            let sum = nums[i] + nums[j] + nums[k];
+
+            if (sum === 0) {
+                res.push([nums[i], nums[j], nums[k]]);
+                while (j < k && nums[j] === nums[j + 1]) {
+                    j++;
+                }
+
+                while (j < k && nums[k] === nums[k - 1]) {
+                    k--;
+                }
+
+                j++;
+                k--;
+
+            } else if (sum < 0) {
+                j++;
+
+            } else {
+                k--;
+            }
+        }
+    }
+
+    return res;
+};
